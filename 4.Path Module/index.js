bcce824667baa56d5d40d77d1fdbfe77354360d0
Muthu404200 <muthu225420@gmail.com>
2025-01-21 09:30:13 +0530

    update notes

--- 4.Path Module/index.js
+++ 4.Path Module/index.js
@@ -9,3 +9,15 @@ import path from "path"
 // console.log(path.extname("c:\\nodejs\\courses\\app.js"));
 
 // console.log(path.join("c","nodejs","courses"));
+
+//GO UP A LEVEL
+console.log(path.join("c:", "huxn-webdev", "courses", "redux-toolkit", ".."))
+console.log(
+path.join("c:", "huxn-webdev", "courses", "redux-toolkit", "..","..")
+);
+
+console.log(path.normalize("c:\\courses\\redux\\store\\features"));
+console.log(path.parse("c:\\nodejs\\courses\\app.js"));
+console. log(path.parse("c:\\nodejs\\courses\\app.js").base) ;
+console. log(path.parse( "c :\\nodejs\\courses\\app.js").name) ;
+console. log(path.parse( "c :\\nodejs\\courses\\app.js").ext)
---
+++ 5.FS Module/1.promise api/index.js
@@ -0,0 +1,73 @@
+import * as fs from "fs/promises";
+
+//creating directory/Folder
+// try{
+//     await fs.mkdir("c:\\nodejs");
+//     console.log("Folder Created ...");
+// }catch(err){
+//     console.log(err);
+// }
+
+// try{
+//     await fs.mkdir("c:\\nodejs\\course",{recursive:true});
+//     console.log("Folder Created ...");
+// }catch(err){
+//     console.log(err);
+// }
+
+//Read  content
+
+// try{
+//     const f1 = await fs.readdir("c:\\nodejs")
+//     for(const file of f1){
+//         console.log(file);
+//     }
+
+// }catch(err){
+//     console.log(err);
+    
+// }
+
+//remove directory/Folder
+// try{
+//     await fs.rmdir("c:\\nodejs\\course");
+//     console.log("Folder Del ...");
+// }catch(err){
+//     console.log(err);
+// }
+
+//Create and write files
+
+// try{
+//     await fs.writeFile("D:\\read.txt","Hello Node")
+//     console.log("File Create");
+// }catch(err){
+//     console.log(err);
+// }
+
+//Read File
+
+// try{
+//     const data = await fs.readFile("D:\\read.txt","utf-8")
+//     console.log(data);
+    
+// }catch(err){
+//     console.log(err);
+// }
+
+//Append files
+
+// try{
+//     await fs.appendFile("D:\\read.txt","Next Express js")
+   
+// }catch(err){
+//     console.log(err);
+// }
+
+//copy File
+try{
+    await fs.copyFile("D:\\read.txt","D:\\Read.md")
+   
+}catch(err){
+    console.log(err);
+}
